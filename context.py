5161966e048c9ada1c2f5bfbed480bf906b98696
sandeepsangole <s_sangole@apple.com>
2024-02-04 14:25:14 -0800

    reverted dotenv changes

--- context.py
+++ context.py
@@ -1,11 +1,9 @@
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 from llama_index import LLMPredictor, PromptHelper, ServiceContext
 from llama_index.indices.postprocessor import SentenceTransformerRerank
-import os
 from langchain_community.llms import OpenAI
-## Add your key here
-os.environ["OPENAI_API_KEY"] ="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"
-os.environ["TOKENIZERS_PARALLELISM"] ="true"
+from dotenv import load_dotenv
+load_dotenv()
 
 llm = OpenAI(model="gpt-3.5-turbo-instruct", temperature=0.5)
 

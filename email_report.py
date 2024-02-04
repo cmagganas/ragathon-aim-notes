2abc75e6e2d81bfa61c997643ca0a8c7ae99557b
JKilpatrick1 <112705716+JKilpatrick1@users.noreply.github.com>
2024-02-04 13:45:24 -0600

    Create email_report.py

---
+++ email_report.py
@@ -0,0 +1,17 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+
+SENDGRID_API_KEY = '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'
+
+def send_email_report(subject, body, to_email):
+    message = MIMEText(body)
+    message['From'] = "meetingminutesai@gmail.com"
+    message['To'] = to_email
+    message['Subject'] = subject
+
+    with smtplib.SMTP('smtp.sendgrid.net', 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.login('apikey', SENDGRID_API_KEY)
+        server.sendmail(from_email, [to_email], message.as_string())

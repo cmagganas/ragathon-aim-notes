324507d149c94cf2a1576dd98b2381e256ff9067
sandeepsangole <s_sangole@apple.com>
2024-02-03 19:48:38 -0800

    Adds streamlit interface to upload recording and view mom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from generate_embeddings import meeting_vector_index
 from context import create_base_context
 import time
+import os
 import logging
 from llama_index import get_response_synthesizer, PromptTemplate, QueryBundle
 from llama_index.response_synthesizers import ResponseMode
@@ -10,6 +11,8 @@ import unicodedata
 from bs4 import BeautifulSoup
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+from langchain import hub
+
 
 #TODO - Try different embedders / models / prompts
 # Note - set your OPEN API key in context.py line 7
@@ -20,19 +23,43 @@ def summerize(dir_name):
     logger.info(" Creating Vector Index")
     storage= meeting_vector_index(dir_name)
     logger.info(" Vector Index Created")
-    return ask_ai(service_context, re_ranker, storage)
 
-def ask_ai(service_context, reranker, index):
-    start_time = time.time()
-    DEFAULT_MOM_TMPL = (
+    action_item_template = (
         "Context information is below. \n"
         "---------------------\n"
         "{context_str}"
         "\n---------------------\n"
-        "Your task is to generate short meeting summary given transcript, action items and high priority or risk items if any."
-        "Use complete output length if needed but don't leave response without closing statement or incomplete sentence."
+        "You are assistant manager to help with meeting follow-up and determine actions participants need to do after."
+        "You will be provided with the meeting transcript."
+        "\nPerform the following tasks:\n1 - Determine topics that are being discussed in the transcript from the meeting, "
+        "which is delimited by triple backticks.\nMake each item short title and one sentence summary."
+        "\n\nFormat your response as a list of topics separated by commas.\n"
+        "2 – Provide list of actions based on the transcript and summary "
+        "with participant name and action description as list of actions "
     )
-    DEFAULT_MOM_TMPL_PROMPT = PromptTemplate(DEFAULT_MOM_TMPL)
+
+    summary_template = (
+        "Context information is below. \n"
+        "---------------------\n"
+        "{context_str}"
+        "\n---------------------\n"
+        "You are assistant manager to help with meeting follow-up and you need to summerize meeting transcript."
+    )
+
+    action_items_res = ask_ai(service_context, re_ranker, storage, action_item_template)
+    summary_template_res = ask_ai(service_context, re_ranker, storage, summary_template)
+
+    return  summary_template_res + action_items_res
+
+def ask_ai(service_context, reranker, index, promt_temp):
+    start_time = time.time()
+
+    # pull a chat prompt
+    prompt = hub.pull("aim-notes/sum-n-act-eval")
+
+    print(prompt)
+
+    DEFAULT_MOM_TMPL_PROMPT = PromptTemplate(promt_temp)
     response_synthesizer = get_response_synthesizer(response_mode=ResponseMode.REFINE, text_qa_template=DEFAULT_MOM_TMPL_PROMPT, service_context=service_context, use_async=True, verbose=True)
     elapsed_time = time.time() - start_time
     logger.info(f"response_synthesizer: {elapsed_time:.2f} seconds")
@@ -40,7 +67,7 @@ def ask_ai(service_context, reranker, index):
     query= "Summarize meeting and share action items?"
     retrieve_re_start_time = time.time()
     query_bundle = QueryBundle(query)
-    retrieved = index.as_query_engine(similarity_top_k=10).retrieve(query_bundle)
+    retrieved = index.as_retriever(similarity_top_k=10).retrieve(query_bundle)
     retrieved_nodes_rank = reranker.postprocess_nodes(retrieved, query_bundle)
     nodes = retrieved_nodes_rank
     logger.info(f"retrieve re-rank: {time.time() - retrieve_re_start_time:.2f} seconds")
@@ -58,5 +85,3 @@ def ask_ai(service_context, reranker, index):
     response = response.replace('\n', '<br>')
     logger.info(f"Processing time: {elapsed_time:.2f} seconds")
     return response
-
-print(summerize("recording_01"))
